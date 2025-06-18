cf675afcd7a8ba9656393f040dcda957e5f367b7
Eliecino Filho <eliecino.efr@gmail.com>
2025-06-18 13:05:57 -0300

    fix: adicionar import do datetime para corrigir erro de NameError

--- auth/models.py
+++ auth/models.py
@@ -2,6 +2,7 @@ from extensions import db, bcrypt
 from flask_login import UserMixin
 from extensions import db
 from flask_bcrypt import bcrypt
+from datetime import datetime
 
 class Usuario(db.Model, UserMixin):
     """
